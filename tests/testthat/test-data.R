d92dcddab8a392f730ea82076c6d2375af27191b
qiushi <qiushi.yann@gmail.com>
2022-06-13 15:15:23 -0500

    update

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,5 +1,5 @@
 test_that("send data works", {
-  h2o_start()
+  h2o::h2o.init()
   on.exit(h2o::h2o.shutdown(prompt = FALSE))
   skip_if(!h2o_running())
 
@@ -8,10 +8,9 @@ test_that("send data works", {
 })
 
 test_that("send data works again", {
-  h2o_start()
+  h2o::h2o.init()
   on.exit(h2o::h2o.shutdown(prompt = FALSE))
   skip_if(!h2o_running())
-
 
   iris_hf <- as.h2o(iris)
   expect_snapshot(iris_hf)
--- tests/testthat/test-model.R
+++ tests/testthat/test-model.R
@@ -1,12 +1,12 @@
 test_that("fitting model works", {
-  h2o_start()
+  h2o::h2o.init()
   on.exit(h2o::h2o.shutdown(prompt = FALSE))
   skip_if(!h2o_running())
 
   bank <- h2o.importFile(
     path ="https://s3.amazonaws.com/h2o-public-test-data/smalldata/demos/bank-additional-full.csv"
   )
-  predictors <- 1:20
+  predictors <- 1:3
   target <- "y"
   mod <- h2o.glm(x = predictors,
                 y = target,
@@ -19,14 +19,14 @@ test_that("fitting model works", {
 
 
 test_that("fitting model works again", {
-  h2o_start()
+  h2o::h2o.init()
   on.exit(h2o::h2o.shutdown(prompt = FALSE))
   skip_if(!h2o_running())
 
   bank <- h2o.importFile(
     path ="https://s3.amazonaws.com/h2o-public-test-data/smalldata/demos/bank-additional-full.csv"
   )
-  predictors <- 1:20
+  predictors <- 1:3
   target <- "y"
   mod <- h2o.glm(x = predictors,
                  y = target,
